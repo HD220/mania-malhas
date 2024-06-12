d5a9b7344d1a167d294febff20280db3a9fd0458
Nicolas Fraga Faust <nicolas.faust@hotmail.com>
2024-06-11 23:18:33 -0300

    alterado para pegar a conexão com bd do local correto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { Pool } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
-import { env } from "process";
-
-const pool = new Pool({ connectionString: env.DATABASE_URL });
-const db = drizzle(pool);
+import db from "@/db";
+import { testeTable } from "@/db/schema/teste";
 
 export default async function Home() {
-  const result = await db.select().from("teste");
+  const result = await db.select().from(testeTable);
+  console.log(result);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
