8779b914bdf584e6d2dffe03cc0fb667e6e485ce
Nicolas Fraga Faust <nicolas.faust@hotmail.com>
2024-06-28 20:08:49 -0300

    teste sem a pasta de components

--- src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/components/forms/login-form";
-
-export default function Page() {
-  return (
-    <div className="flex min-h-screen flex-1 items-center justify-center p-6">
-      <LoginForm />
-    </div>
-  );
-}
