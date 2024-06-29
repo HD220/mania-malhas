a1a67b1ef99133c55ed0f043dec01ba6263eeeee
Nicolas Fraga Faust <nicolas.faust@hotmail.com>
2024-06-28 21:06:48 -0300

    nuss

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -27,16 +27,16 @@ export async function uploadS3(
   });
 }
 
-// function arrayBufferToArray(buffer: ArrayBuffer) {
-//   return Array.from(new Uint8Array(buffer));
-// }
+function arrayBufferToArray(buffer: ArrayBuffer) {
+  return Array.from(new Uint8Array(buffer));
+}
 
-// function arrayToHex(array: number[]) {
-//   return array.map((byte) => byte.toString(16).padStart(2, "0")).join("");
-// }
+function arrayToHex(array: number[]) {
+  return array.map((byte) => byte.toString(16).padStart(2, "0")).join("");
+}
 
-// export async function createHashFromFile(file: File) {
-//   const buffer = Buffer.from(await file.arrayBuffer());
-//   const hashBuffer = await window.crypto.subtle.digest("SHA-256", buffer);
-//   return arrayToHex(arrayBufferToArray(hashBuffer));
-// }
+export async function createHashFromFile(file: File) {
+  const buffer = Buffer.from(await file.arrayBuffer());
+  const hashBuffer = await window.crypto.subtle.digest("SHA-256", buffer);
+  return arrayToHex(arrayBufferToArray(hashBuffer));
+}
